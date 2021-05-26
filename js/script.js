d66d0ceabe8a6ec85fc10bb8564aec2515e83f26
HJRandolph <hjrandolph@gmail.com>
2021-05-26 12:07:23 -0400

    Pulling in repo list and displaying on page.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const overview = document.querySelector(".overview");
 const username = 'hjrandolph';
+const repoList = document.querySelector(".repo-list");
 
 const getUser = async function(){
     const res = await fetch(`https://api.github.com/users/${username}`);
     const user = await res.json();
     console.log(user);
     displayUser(user);
+ 
 };
 
 getUser();
@@ -22,13 +24,27 @@ const displayUser = function(user){
         <p><strong>Number of public repos:</strong> ${user.public_repos}</p>
     </div>`;
     overview.appendChild(userInfo);
+    fetchRepos();
 };
 
+const fetchRepos = async function(){
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=12`);
+    const repos = await res.json();
+    console.log(repos);
+    displayRepos(repos)
+};
 
-// Create and name an async function to fetch information from your GitHub 
-// profile using the GitHub API address: https://api.github.com. Target the 
-// "users" endpoint and use a template literal to add the global username 
-// variable to the endpoint: users/${username}. 
-// Notice that you'll add a "$" character in front of the variable name to 
-// create a placeholder. Because you're using a template literal, surround 
-// the URL in backticks instead of quotation marks.
+const displayRepos = function(repos){
+    for (const repo of repos){
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML =  `<h3>${repo.name}</h3>`
+        repoList.append(li);
+    }
+};
+
+// for (const letter of guessedLetters){
+//     const li = document.createElement("li");
+//     li.innerText = letter;
+//     guessed.append(li);
+// }
