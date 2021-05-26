ad45f7013f9a1f6d020a2e6ca0ce6c0504afcf91
HJRandolph <hjrandolph@gmail.com>
2021-05-26 16:58:00 -0400

    Pulling in repo data, displaying it, and linking to repo.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const overview = document.querySelector(".overview");
 const username = 'hjrandolph';
 const repoList = document.querySelector(".repo-list");
+const reposSection = document.querySelector(".repos");
+const repoData = document.querySelector(".repo-data");
+
 
 const getUser = async function(){
     const res = await fetch(`https://api.github.com/users/${username}`);
     const user = await res.json();
-    console.log(user);
+    // console.log(user);
     displayUser(user);
  
 };
@@ -43,8 +46,48 @@ const displayRepos = function(repos){
     }
 };
 
-// for (const letter of guessedLetters){
-//     const li = document.createElement("li");
-//     li.innerText = letter;
-//     guessed.append(li);
-// }
+
+repoList.addEventListener("click", function(e){
+    if (e.target.matches("h3")){
+        const repoName = e.target.innerText;
+        // console.log(repoName);
+        specificRepo(repoName);
+    }
+    
+});
+
+const specificRepo = async function(repoName){
+    const res = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await res.json();
+    console.log(repoInfo);
+    const fetchLanguages = await fetch(`https://api.github.com/repos/${username}/${repoName}/languages`);
+    const languageData = await fetchLanguages.json();
+    console.log(languageData);
+    const languages = [];
+    for (let key in languageData){
+        languages.push(key);
+    }
+    displaySpecificRepo(repoInfo,languages);
+};
+
+const displaySpecificRepo = function(repoInfo, languages){
+    repoData.innerText = "";
+    const repo = document.createElement("div");
+    repo.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(', ')} </p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" re'="noreferrer noopener">View repo on GitHub!</a>`;
+    repoData.append(repo);
+    repoData.classList.remove("hide");
+};
+
+//     Create a new div element and add the selected repository's name, description, default branch, and link to its code on GitHub. 
+// The div structure will look like this:
+
+	
+// <h3>Name: ${}</h3>
+//     <p>Description: ${}</p>
+//     <p>Default Branch: ${}</p>
+//     <p>Languages: ${languages.join(", ")}</p>
+//     <a class="visit" href="${}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
